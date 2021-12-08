329a1caa4f047eb926e99ad251fc09305a377db8
NoronhaGCP <inc.691@gmail.com>
2021-12-08 17:19:34 -0300

    start frontend project

---
+++ frontend/pages/index.tsx
@@ -0,0 +1,28 @@
+import type { NextPage } from 'next';
+import { FormEvent, useState } from 'react';
+
+const Home: NextPage = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+
+    const data = {
+      email,
+      password,
+    };
+
+    console.log(data);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input type='email' value={email} onChange={(e) => setEmail(e.target.value)} />
+      <input type='password' value={password} onChange={(e) => setPassword(e.target.value)} />
+      <button type='submit'>Entrar</button>
+    </form>
+  );
+};
+
+export default Home;
