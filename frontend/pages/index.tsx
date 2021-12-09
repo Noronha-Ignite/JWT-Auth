fc1c7c866fac7d1d63f62aa8df9c77d4729ea5d2
NoronhaGCP <inc.691@gmail.com>
2021-12-08 23:00:28 -0300

    add basic container style to form

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,6 +1,9 @@
 import type { NextPage } from 'next';
 import { FormEvent, useState } from 'react';
+
 import { useAuth } from '../contexts/AuthContext';
+
+import styles from '../styles/Home.module.css';
 
 const Home: NextPage = () => {
   const [email, setEmail] = useState('');
@@ -20,7 +23,7 @@ const Home: NextPage = () => {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
+    <form className={styles.container} onSubmit={handleSubmit}>
       <input type='email' value={email} onChange={(e) => setEmail(e.target.value)} />
       <input type='password' value={password} onChange={(e) => setPassword(e.target.value)} />
       <button type='submit'>Entrar</button>
