55d7dce09637a9106da61f346e8cb16cc451e565
NoronhaGCP <inc.691@gmail.com>
2021-12-09 21:11:25 -0300

    can only access login page when not logged

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,9 +1,11 @@
-import type { NextPage } from 'next';
+import type { GetServerSideProps, NextPage } from 'next';
+import { parseCookies } from 'nookies';
 import { FormEvent, useState } from 'react';
 
 import { useAuth } from '../contexts/AuthContext';
 
 import styles from '../styles/Home.module.css';
+import { withSSRGuest } from '../utils/withSSRGuest';
 
 const Home: NextPage = () => {
   const [email, setEmail] = useState('');
@@ -32,3 +34,9 @@ const Home: NextPage = () => {
 };
 
 export default Home;
+
+export const getServerSideProps = withSSRGuest(async (ctx) => {
+  return {
+    props: {},
+  };
+});
